1c4eb8cd4a838e36c712ab785325b5b2c250106a
an.kharchenko <an.kharchenko@hexa.com.ua>
2020-01-10 14:20:54 +0000

    Some notes for README.md [2]

--- snoozegate.py
+++ snoozegate.py
@@ -1,39 +1,39 @@
-# Just code
-# GUI
-# Drone actions modulation
+Just code
+GUI
+Drone actions modulation
 
 
-# Signals library
-# Encoder and decoder
-# Few levels of noised environments
-# Modulation of drone actions
+Signals library
+Encoder and decoder
+Few levels of noised environments
+Modulation of drone actions
 
 
-# Inputting the signal
-# Encoding the signal
-# Stressing the signal (noised environment)
-# Decoding the signal
-# Sending the signal to the drone
+Inputting the signal
+Encoding the signal
+Stressing the signal (noised environment)
+Decoding the signal
+Sending the signal to the drone
 
 
-### Signals library:
-#
-## Actions:
-# Only binary values
-# in 4 binary bits
-# 00001111 - dry-down
-# 00001000 - dry-up
-# 00000100 - dry-left
-# 00000010 - dry-right
-# 11110100 - left+down
-# 10000100 - left+up
-# 11110010 - right+down
-# 10000010 - right+up
-#
+# Signals library:
+
+# Actions:
+Only binary values
+in 4 binary bits
+00001111 - dry-down
+00001000 - dry-up
+00000100 - dry-left
+00000010 - dry-right
+11110100 - left+down
+10000100 - left+up
+11110010 - right+down
+10000010 - right+up
+
 ## Values:
-# Only positive integer values in degrees
-# from 0 to 180
-# would be converted into 8-bit binary
+Only positive integer values in degrees
+from 0 to 180
+would be converted into 8-bit binary
 
 
 signal_lib = ['0000', '1000', '0100', '0010',
