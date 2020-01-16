ed16a312119fd964833f46c3fecc98f00633db70
an.kharchenko <an.kharchenko@hexa.com.ua>
2020-01-16 16:13:38 +0000

    Shitworking on environment - deadly progress T_T

--- snoozegate.py
+++ snoozegate.py
@@ -38,6 +38,7 @@
 
 # import pylab
 # import numpy
+import re
 import random
 
 
@@ -54,9 +55,9 @@ signal_lib = ['10001111',
 
 def input_check():
     # signal = input('Enter the signal here: ')
-    value = int(input('Determine the value from 0 to 90 here: '))
-    signal = '10001111'
-    # value = int('31')
+    # value = int(input('Determine the value from 0 to 90 here: '))
+    signal = '10001001'
+    value = int('31')
     if signal in signal_lib and value in range(0, 91):
         # print(signal,value)
         return signal, value
@@ -87,20 +88,156 @@ def encoder(signal,value):
     return sig, val
 
 
-def environment(signal, value):
+
+
+def zeroes_environment(signal, value):
+
     s = list(signal)
     v = list(value)
-    for index,item in enumerate(s):
-        print(index,item)
+
+    matrix_s = []
+    matrix_v = []
+
+    for index, item in enumerate(s, 1):
+        mask1 = [index,item]
+        matrix_s.append(mask1)
 
 
-    print(s,v)
+    for index, item in enumerate(v, 1):
+        mask2 = [index,item]
+        matrix_v.append(mask2)
+
+    A = '1'
+    N = 0
+    while (N < (len(matrix_s)-1)):
+        z1 = random.randint(0, len(matrix_s) - 1)
+        k1 = matrix_s[z1][1]
+        if (k1 == '0'):
+            N = len(matrix_s)
+            mask_m = []
+            mask_m.append(z1)
+            mask_m.append(A)
+            if (z1 == 0):
+                matrix_s[z1].clear()
+                matrix_s[z1].extend(mask_m)
+            elif (z1 > 0):
+                matrix_s[z1 - 1].clear()
+                matrix_s[z1 - 1].extend(mask_m)
+            L = 0
+            result_mm = []
+            while L < len(matrix_s):
+                result_mm.append(matrix_s[L][1])
+                L = L + 1
+            result_m = ''.join(result_mm)
+            print(result_m)
+        else:
+            pass
+
+
+    A = '1'
+    N = 0
+    while (N < (len(matrix_v)-1)):
+        z2 = random.randint(0, len(matrix_v) - 1)
+        k2 = matrix_v[z2][1]
+        if (k2 == '0'):
+            N = len(matrix_v)
+            mask_v = []
+            mask_v.append(z2)
+            mask_v.append(A)
+            if (z2 == 0):
+                matrix_v[z2].clear()
+                matrix_v[z2].extend(mask_v)
+            elif (z2 > 0):
+                matrix_v[z2 - 1].clear()
+                matrix_v[z2 - 1].extend(mask_v)
+            L = 0
+            result_mv = []
+            while L < len(matrix_v):
+                result_mv.append(matrix_v[L][1])
+                L = L + 1
+            result_v = ''.join(result_mv)
+            print(result_v)
+        else:
+            pass
+
+
+def ones_environment(signal, value):
+
+    s = list(signal)
+    v = list(value)
+
+    matrix_s = []
+    matrix_v = []
+
+    for index, item in enumerate(s, 1):
+        mask1 = [index,item]
+        matrix_s.append(mask1)
+
+
+    for index, item in enumerate(v, 1):
+        mask2 = [index,item]
+        matrix_v.append(mask2)
+
+    A = '0'
+    N = 0
+    while (N < (len(matrix_s)-1)):
+        z1 = random.randint(0, len(matrix_s) - 1)
+        k1 = matrix_s[z1][1]
+        if (k1 == '1'):
+            N = len(matrix_s)
+            mask_m = []
+            mask_m.append(z1)
+            mask_m.append(A)
+            if (z1 == 0):
+                matrix_s[z1].clear()
+                matrix_s[z1].extend(mask_m)
+            elif (z1 > 0):
+                matrix_s[z1 - 1].clear()
+                matrix_s[z1 - 1].extend(mask_m)
+            L = 0
+            result_mm = []
+            while L < len(matrix_s):
+                result_mm.append(matrix_s[L][1])
+                L = L + 1
+            result_m = ''.join(result_mm)
+            print(result_m)
+        else:
+            pass
+
+
+    A = '0'
+    N = 0
+    while (N < (len(matrix_v)-1)):
+        z2 = random.randint(0, len(matrix_v) - 1)
+        k2 = matrix_v[z2][1]
+        if (k2 == '1'):
+            N = len(matrix_v)
+            mask_v = []
+            mask_v.append(z2)
+            mask_v.append(A)
+            if (z2 == 0):
+                matrix_v[z2].clear()
+                matrix_v[z2].extend(mask_v)
+            elif (z2 > 0):
+                matrix_v[z2 - 1].clear()
+                matrix_v[z2 - 1].extend(mask_v)
+            L = 0
+            result_mv = []
+            while L < len(matrix_v):
+                result_mv.append(matrix_v[L][1])
+                L = L + 1
+            result_v = ''.join(result_mv)
+            print(result_v)
+        else:
+            pass
+
 
 
 def main():
     signal, value = input_check()
     out_sig, out_val = encoder(signal, value)
-    environment(out_sig, out_val)
+    zeroes_environment(out_sig, out_val)
+    ones_environment(out_sig, out_val)
 
 
 if __name__ == main():
